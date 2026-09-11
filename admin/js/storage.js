3d2555a0f3b7fb9661256b9cc0df346beea4bafe
Expert Corporate Solutions <dharman@expertcorporatesolutions.com>
2026-09-11 11:38:43 +0530

    Update storage.js

--- admin/js/storage.js
+++ admin/js/storage.js
@@ -1,39 +1,42 @@
 window.CMSStorage = {
 
-  uploadImage: async function(file, folder, progressCallback) {
+    available: function() {
+        return true;
+    },
 
-    const CLOUD_NAME = "zegwar3g";
-    const UPLOAD_PRESET = "excoso_products";
+    uploadImage: async function(file, folder, progressCallback) {
 
-    const formData = new FormData();
+        const CLOUD_NAME = "YOUR_CLOUDINARY_NAME";
+        const UPLOAD_PRESET = "YOUR_UPLOAD_PRESET";
 
-    formData.append("file", file);
-    formData.append("upload_preset", UPLOAD_PRESET);
+        const formData = new FormData();
 
-    const response = await fetch(
-      `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`,
-      {
-        method: "POST",
-        body: formData
-      }
-    );
+        formData.append("file", file);
+        formData.append("upload_preset", UPLOAD_PRESET);
 
-    const data = await response.json();
+        const response = await fetch(
+            `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`,
+            {
+                method: "POST",
+                body: formData
+            }
+        );
 
-    if (!data.secure_url) {
-      throw new Error("Upload failed");
+        const data = await response.json();
+
+        if (!data.secure_url) {
+            throw new Error("Upload failed");
+        }
+
+        if (progressCallback) {
+            progressCallback(100);
+        }
+
+        return data.secure_url;
+    },
+
+    deleteImage: async function(url) {
+        return true;
     }
-
-    if (progressCallback) {
-      progressCallback(100);
-    }
-
-    return data.secure_url;
-  },
-
-  deleteImage: async function(url) {
-    console.log("Cloudinary image delete skipped:", url);
-    return true;
-  }
 
 };
