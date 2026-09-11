254125264e8a976b7e830c76fa7571e220c9bc18
Expert Corporate Solutions <dharman@expertcorporatesolutions.com>
2026-09-11 11:09:51 +0530

    Update cloudinary.js

--- admin/js/cloudinary.js
+++ admin/js/cloudinary.js
@@ -11,7 +11,7 @@ export async function uploadImage(file) {
     formData.append("upload_preset", UPLOAD_PRESET);
 
     const response = await fetch(
-        `https://api.cloudinary.com/v1_1/${zegwar3g}/image/upload`,
+        `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`,
         {
             method: "POST",
             body: formData
